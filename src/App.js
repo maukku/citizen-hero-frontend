beb4e129b5b2c3fe201fdfd2ed4232d3950b574a
Mauro <maurorissocee@gmail.com>
2022-06-01 18:20:58 +0200

    Representative Component Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
+import Representative from "./components/RepresentativeItem";
 function App() {
   return (
-    <>
-      <h1 className="header">Citizen Hero</h1>
-    </>
+    <div className="App">
+      <section className="App-content">
+        <h1 className="header">Citizen Hero</h1>
+        <Representative />
+      </section>
+    </div>
   );
 }
 

dd49c4aee04fe4a67cbfbb633e3959ff265a8c53
Mauro <maurorissocee@gmail.com>
2022-06-13 20:40:56 +0200

    Header added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import SidebarData from './SidebarData'
+import React from "react";
+import SidebarData from "./SidebarData";
 
 function Sidebar() {
   return (
-    <div className='Sidebar'>
-      <ul className='SidebarList'>
-      {SidebarData.map(
-   (val , key)=> {
-   return(
-<li key={key}  className="row" 
-id={window.location.pathname == val.link ? "active" : ""}
-onClick ={()=>{
-window.location.pathname=val.link
-
-}} >
-<div id ="sidebarIcon">{val.icon}</div>
-<div id="sidebarTitle">{val.title}</div>
-<br/>
-</li>
-   ) 
-   }
-
-    )}
-    </ul>
+    <div className="Sidebar">
+      <ul className="SidebarList">
+        {SidebarData.map((val, key) => {
+          return (
+            <li
+              key={key}
+              className="row"
+              id={window.location.pathname === val.link ? "active" : ""}
+              onClick={() => {
+                window.location.pathname = val.link;
+              }}
+            >
+              <div id="sidebarIcon">{val.icon}</div>
+              <div id="sidebarTitle">{val.title}</div>
+              <br />
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;

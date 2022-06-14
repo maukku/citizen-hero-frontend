0a5d63c7c8a2aa51d9d846d98974b67125c9df7a
Mauro <maurorissocee@gmail.com>
2022-06-14 12:29:45 +0200

    Pages Added

---
+++ src/components/pages/Politicians.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Politicians() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Politicians
---
+++ src/components/pages/Votes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Votes() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Votes

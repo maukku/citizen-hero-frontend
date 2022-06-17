7295b6159526b78d7f2e284cbbace85bf0289a7d
Mauro <maurorissocee@gmail.com>
2022-06-18 01:30:56 +0200

    new footer added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Sidebar from "./components/shared/Sidebar";
 import Footer from "./components/shared/Footer";
-
+import Contact from "./components/pages/Contact";
+import Legal from "./components/pages/Legal";
+import Privacy from "./components/pages/Privacy";
 import Home from "./components/pages/Home";
 import Votes from "./components/pages/Votes";
 import Politicians from "./components/pages/Politicians";
@@ -32,13 +34,16 @@ function App() {
         <Header />
         <div className="main-container">
           <main className="content">
-          <Routes >
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/Votes" element={<Votes />} />
-            <Route exact path="/Politicians" element={<Politicians />} />
-            <Route exact path="/Login" element={<Login />} />
-          </Routes>
-</main>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/Votes" element={<Votes />} />
+              <Route exact path="/Politicians" element={<Politicians />} />
+              <Route exact path="/Login" element={<Login />} />
+              <Route exact path="/Legal" element={<Legal />} />
+              <Route exact path="/Contact" element={<Contact />} />
+              <Route exact path="/Privacy" element={<Privacy />} />
+            </Routes>
+          </main>
           <Sidebar />
         </div>
         <Footer />
---
+++ src/components/pages/Legal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Legal() {
+  return (
+    <div>
+      <h1>Protect me !</h1>
+    </div>
+  );
+}
+
+export default Legal;

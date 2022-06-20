d8481765187750c3f5f0a96b26898bcc1ae5664f
Mauro <maurorissocee@gmail.com>
2022-06-21 01:08:21 +0200

    Footer position improved

--- src/components/Representative.jsx
+++ src/components/Representative.jsx
@@ -1,12 +1,13 @@
 function Representative({ representative }) {
   return (
     <div className="Representative">
+      <div className="Representative-img">
         <img
           className="Representative-img"
           src={representative.imageUrl}
           alt=""
         />
-
+      </div>
       <div className="Representative-text">
         <div>{representative.fName + " " + representative.lName}</div>
         <div>{representative.partei}</div>

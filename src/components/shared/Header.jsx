2085d1de10f447103e06e6031dbb458f8a1065a4
Mauro <maurorissocee@gmail.com>
2022-06-21 17:01:05 +0200

    Representative's Context implemented

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -4,11 +4,11 @@ function Header() {
     <div className="Header">
       <img
         className="Header-img"
-        src="https://czholding.ru/wp-content/uploads/2019/06/hands-600497_960_720.jpg"
+        src="https://cdn.pixabay.com/photo/2020/02/29/22/39/demonstration-4891278_960_720.jpg"
         alt="hands"
       />
       <Link to="/">
-        <h1 className="Header-h1">CITIZEN HERO</h1>
+        <h1 className="Header-h1">CITIZEN HERO</h1>{" "}
       </Link>
     </div>
   );

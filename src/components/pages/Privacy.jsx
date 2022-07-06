d413d9ce6a4aa9b52e9ddb2031d34368140ff456
Mauro <maurorissocee@gmail.com>
2022-07-06 16:01:30 +0200

    Privacy text added

--- src/components/pages/Privacy.jsx
+++ src/components/pages/Privacy.jsx
@@ -2,8 +2,333 @@ import React from "react";
 
 function Privacy() {
   return (
-    <div>
-      <h1>Privacy is important</h1>
+    <div className="Privacy FooterPage">
+      <h1>Datenschutzerklärung</h1>
+      <p>
+        Verantwortlicher im Sinne der Datenschutzgesetze, insbesondere der
+        EU-Datenschutzgrundverordnung (DSGVO), ist:
+      </p>
+      <p>Till Leinen</p>
+      <h2>Ihre Betroffenenrechte</h2>
+      <p>
+        Unter den angegebenen Kontaktdaten unseres Datenschutzbeauftragten
+        können Sie jederzeit folgende Rechte ausüben:
+      </p>
+      <ul>
+        <li>
+          Auskunft über Ihre bei uns gespeicherten Daten und deren Verarbeitung
+          (Art. 15 DSGVO),
+        </li>
+        <li>
+          Berichtigung unrichtiger personenbezogener Daten (Art. 16 DSGVO),
+        </li>
+        <li>Löschung Ihrer bei uns gespeicherten Daten (Art. 17 DSGVO),</li>
+        <li>
+          Einschränkung der Datenverarbeitung, sofern wir Ihre Daten aufgrund
+          gesetzlicher Pflichten noch nicht löschen dürfen (Art. 18 DSGVO),
+        </li>
+        <li>
+          Widerspruch gegen die Verarbeitung Ihrer Daten bei uns (Art. 21 DSGVO)
+          und
+        </li>
+        <li>
+          Datenübertragbarkeit, sofern Sie in die Datenverarbeitung eingewilligt
+          haben oder einen Vertrag mit uns abgeschlossen haben (Art. 20 DSGVO).
+        </li>
+      </ul>
+      <p>
+        Sofern Sie uns eine Einwilligung erteilt haben, können Sie diese
+        jederzeit mit Wirkung für die Zukunft widerrufen.
+      </p>
+      <p>
+        Sie können sich jederzeit mit einer Beschwerde an eine Aufsichtsbehörde
+        wenden, z. B. an die zuständige Aufsichtsbehörde des Bundeslands Ihres
+        Wohnsitzes oder an die für uns als verantwortliche Stelle zuständige
+        Behörde.
+      </p>
+      <p>
+        Eine Liste der Aufsichtsbehörden (für den nichtöffentlichen Bereich) mit
+        Anschrift finden Sie unter:{" "}
+        <a
+          href="https://www.bfdi.bund.de/DE/Service/Anschriften/Laender/Laender-node.html"
+          target="_blank"
+          rel="noopener nofollow"
+        >
+          https://www.bfdi.bund.de/DE/Service/Anschriften/Laender/Laender-node.html
+        </a>
+        .
+      </p>
+      <p></p>
+      <h2>Cookies</h2>
+      <p>
+        Wie viele andere Webseiten verwenden wir auch so genannte „Cookies“. Bei
+        Cookies handelt es sich um kleine Textdateien, die auf Ihrem Endgerät
+        (Laptop, Tablet, Smartphone o.ä.) gespeichert werden, wenn Sie unsere
+        Webseite besuchen.{" "}
+      </p>
+      <p>
+        Sie können Sie einzelne Cookies oder den gesamten Cookie-Bestand
+        löschen. Darüber hinaus erhalten Sie Informationen und Anleitungen, wie
+        diese Cookies gelöscht oder deren Speicherung vorab blockiert werden
+        können. Je nach Anbieter Ihres Browsers finden Sie die notwendigen
+        Informationen unter den nachfolgenden Links:
+      </p>
+      <ul>
+        <li>
+          Mozilla Firefox:{" "}
+          <a
+            href="https://support.mozilla.org/de/kb/cookies-loeschen-daten-von-websites-entfernen"
+            target="_blank"
+            rel="nofollow noopener"
+          >
+            https://support.mozilla.org/de/kb/cookies-loeschen-daten-von-websites-entfernen
+          </a>
+        </li>
+        <li>
+          Internet Explorer:{" "}
+          <a
+            href="https://support.microsoft.com/de-de/help/17442/windows-internet-explorer-delete-manage-cookies"
+            target="_blank"
+            rel="nofollow noopener"
+          >
+            https://support.microsoft.com/de-de/help/17442/windows-internet-explorer-delete-manage-cookies
+          </a>
+        </li>
+        <li>
+          Google Chrome:{" "}
+          <a
+            href="https://support.google.com/accounts/answer/61416?hl=de"
+            target="_blank"
+            rel="nofollow noopener"
+          >
+            https://support.google.com/accounts/answer/61416?hl=de
+          </a>
+        </li>
+        <li>
+          Opera:{" "}
+          <a
+            href="http://www.opera.com/de/help"
+            target="_blank"
+            rel="nofollow noopener"
+          >
+            http://www.opera.com/de/help
+          </a>
+        </li>
+        <li>
+          Safari:{" "}
+          <a
+            href="https://support.apple.com/kb/PH17191?locale=de_DE&viewlocale=de_DE"
+            target="_blank"
+            rel="nofollow noopener"
+          >
+            https://support.apple.com/kb/PH17191?locale=de_DE&viewlocale=de_DE
+          </a>
+        </li>
+      </ul>
+      <h3>Speicherdauer und eingesetzte Cookies:</h3>
+      <p>
+        Soweit Sie uns durch Ihre Browsereinstellungen oder Zustimmung die
+        Verwendung von Cookies erlauben, können folgende Cookies auf unseren
+        Webseiten zum Einsatz kommen:
+      </p>
+      <p></p>
+      <h2>Technisch notwendige Cookies </h2>
+      <h3>Art und Zweck der Verarbeitung: </h3>
+      <p>
+        Wir setzen Cookies ein, um unsere Website nutzerfreundlicher zu
+        gestalten. Einige Elemente unserer Internetseite erfordern es, dass der
+        aufrufende Browser auch nach einem Seitenwechsel identifiziert werden
+        kann.
+      </p>
+      <p>
+        Der Zweck der Verwendung technisch notwendiger Cookies ist, die Nutzung
+        von Websites für die Nutzer zu vereinfachen. Einige Funktionen unserer
+        Internetseite können ohne den Einsatz von Cookies nicht angeboten
+        werden. Für diese ist es erforderlich, dass der Browser auch nach einem
+        Seitenwechsel wiedererkannt wird.
+      </p>
+      <p>Für folgende Anwendungen benötigen wir Cookies:</p>
+      <p>
+        Übernahme von Spracheinstellungen
+        <br />
+        Merken von Suchbegriffen
+      </p>
+      <h3>Rechtsgrundlage und berechtigtes Interesse: </h3>
+      <p>
+        Die Verarbeitung erfolgt gemäß Art. 6 Abs. 1 lit. f DSGVO auf Basis
+        unseres berechtigten Interesses an einer nutzerfreundlichen Gestaltung
+        unserer Website.
+      </p>
+      <h3>Empfänger: </h3>
+      <p>
+        Empfänger der Daten sind ggf. technische Dienstleister, die für den
+        Betrieb und die Wartung unserer Website als Auftragsverarbeiter tätig
+        werden.
+      </p>
+      <p></p>
+      <h3>Bereitstellung vorgeschrieben oder erforderlich:</h3>
+      <p>
+        Die Bereitstellung der vorgenannten personenbezogenen Daten ist weder
+        gesetzlich noch vertraglich vorgeschrieben. Ohne diese Daten ist jedoch
+        der Dienst und die Funktionsfähigkeit unserer Website nicht
+        gewährleistet. Zudem können einzelne Dienste und Services nicht
+        verfügbar oder eingeschränkt sein.
+      </p>
+      <h3>Widerspruch</h3>
+      <p>
+        Lesen Sie dazu die Informationen über Ihr Widerspruchsrecht nach Art. 21
+        DSGVO weiter unten.
+      </p>
+      <p></p>
+      <h2>Registrierung auf unserer Website</h2>
+      <h3>Art und Zweck der Verarbeitung:</h3>
+      <p>
+        Für die Registrierung auf unserer Website benötigen wir einige
+        personenbezogene Daten, die über eine Eingabemaske an uns übermittelt
+        werden.{" "}
+      </p>
+      <p>
+        Zum Zeitpunkt der Registrierung werden zusätzlich folgende Daten
+        erhoben:
+      </p>
+      <p></p>
+      <p>
+        Ihre Registrierung ist für das Bereithalten bestimmter Inhalte und
+        Leistungen auf unserer Website erforderlich.
+      </p>
+      <h3>Rechtsgrundlage:</h3>
+      <p>
+        Die Verarbeitung der bei der Registrierung eingegebenen Daten erfolgt
+        auf Grundlage einer Einwilligung des Nutzers (Art. 6 Abs. 1 lit. a
+        DSGVO).
+      </p>
+      <h3>Empfänger:</h3>
+      <p>
+        Empfänger der Daten sind ggf. technische Dienstleister, die für den
+        Betrieb und die Wartung unserer Website als Auftragsverarbeiter tätig
+        werden.
+      </p>
+      <p></p>
+      <h3>Speicherdauer:</h3>
+      <p>
+        Daten werden in diesem Zusammenhang nur verarbeitet, solange die
+        entsprechende Einwilligung vorliegt.{" "}
+      </p>
+      <h3>Bereitstellung vorgeschrieben oder erforderlich:</h3>
+      <p>
+        Die Bereitstellung Ihrer personenbezogenen Daten erfolgt freiwillig,
+        allein auf Basis Ihrer Einwilligung. Ohne die Bereitstellung Ihrer
+        personenbezogenen Daten können wir Ihnen keinen Zugang auf unsere
+        angebotenen Inhalte gewähren.{" "}
+      </p>
+      <p></p>
+      <h2>Newsletter</h2>
+      <h3>Art und Zweck der Verarbeitung:</h3>
+      <p>
+        Für die Zustellung unseres Newsletters erheben wir personenbezogene
+        Daten, die über eine Eingabemaske an uns übermittelt werden.
+      </p>
+      <p>
+        Für eine wirksame Registrierung benötigen wir eine valide
+        E-Mail-Adresse. Um zu überprüfen, dass eine Anmeldung tatsächlich durch
+        den Inhaber einer E-Mail-Adresse erfolgt, setzen wir das
+        „Double-Opt-in“-Verfahren ein. Hierzu protokollieren wir die Anmeldung
+        zum Newsletter, den Versand einer Bestätigungsmail und den Eingang der
+        hiermit angeforderten Antwort. Weitere Daten werden nicht erhoben.{" "}
+      </p>
+      <h3>Rechtsgrundlage:</h3>
+      <p>
+        Auf Grundlage Ihrer ausdrücklich erteilten Einwilligung (Art. 6 Abs. 1
+        lit. a DSGVO), übersenden wir Ihnen regelmäßig unseren Newsletter bzw.
+        vergleichbare Informationen per E-Mail an Ihre angegebene
+        E-Mail-Adresse.{" "}
+      </p>
+      <p>
+        Die Einwilligung zur Speicherung Ihrer persönlichen Daten und ihrer
+        Nutzung für den Newsletterversand können Sie jederzeit mit Wirkung für
+        die Zukunft widerrufen. In jedem Newsletter findet sich dazu ein
+        entsprechender Link. Außerdem können Sie sich jederzeit auch direkt auf
+        dieser Website abmelden oder uns Ihren Widerruf über die am Ende dieser
+        Datenschutzhinweise angegebene Kontaktmöglichkeit mitteilen.
+      </p>
+      <h3>Empfänger:</h3>
+      <p>Empfänger der Daten sind ggf. Auftragsverarbeiter.</p>
+      <p></p>
+      <h3>Speicherdauer:</h3>
+      <p>
+        Die Daten werden in diesem Zusammenhang nur verarbeitet, solange die
+        entsprechende Einwilligung vorliegt. Danach werden sie gelöscht.
+      </p>
+      <h3>Bereitstellung vorgeschrieben oder erforderlich:</h3>
+      <p>
+        Die Bereitstellung Ihrer personenbezogenen Daten erfolgt freiwillig,
+        allein auf Basis Ihrer Einwilligung. Ohne bestehende Einwilligung können
+        wir Ihnen unseren Newsletter leider nicht zusenden.
+      </p>
+      <h3>Widerruf der Einwilligung:</h3>
+      <p>
+        Die Einwilligung zur Speicherung Ihrer persönlichen Daten und ihrer
+        Nutzung für den Newsletterversand können Sie jederzeit mit Wirkung für
+        die Zukunft widerrufen. Die Abmeldung kann über den in jeder E-Mail
+        enthaltenen Link oder beim unten aufgeführten Datenschutzbeauftragten
+        bzw. der für den Datenschutz zuständigen Person beantragt werden.{" "}
+      </p>
+      <p></p>
+      <h2>SSL-Verschlüsselung</h2>
+      <p>
+        Um die Sicherheit Ihrer Daten bei der Übertragung zu schützen, verwenden
+        wir dem aktuellen Stand der Technik entsprechende
+        Verschlüsselungsverfahren (z. B. SSL) über HTTPS.
+      </p>
+      <p></p>
+      <h2>Information über Ihr Widerspruchsrecht nach Art. 21 DSGVO</h2>
+      <h3>Einzelfallbezogenes Widerspruchsrecht</h3>
+      <p>
+        Sie haben das Recht, aus Gründen, die sich aus Ihrer besonderen
+        Situation ergeben, jederzeit gegen die Verarbeitung Sie betreffender
+        personenbezogener Daten, die aufgrund Art. 6 Abs. 1 lit. f DSGVO
+        (Datenverarbeitung auf der Grundlage einer Interessenabwägung) erfolgt,
+        Widerspruch einzulegen; dies gilt auch für ein auf diese Bestimmung
+        gestütztes Profiling im Sinne von Art. 4 Nr. 4 DSGVO.
+      </p>
+      <p>
+        Legen Sie Widerspruch ein, werden wir Ihre personenbezogenen Daten nicht
+        mehr verarbeiten, es sei denn, wir können zwingende schutzwürdige Gründe
+        für die Verarbeitung nachweisen, die Ihre Interessen, Rechte und
+        Freiheiten überwiegen, oder die Verarbeitung dient der Geltendmachung,
+        Ausübung oder Verteidigung von Rechtsansprüchen.
+      </p>
+      <h3>Empfänger eines Widerspruchs</h3>
+      <p>Till Leinen</p>
+      <h2>Änderung unserer Datenschutzbestimmungen</h2>
+      <p>
+        Wir behalten uns vor, diese Datenschutzerklärung anzupassen, damit sie
+        stets den aktuellen rechtlichen Anforderungen entspricht oder um
+        Änderungen unserer Leistungen in der Datenschutzerklärung umzusetzen,
+        z.B. bei der Einführung neuer Services. Für Ihren erneuten Besuch gilt
+        dann die neue Datenschutzerklärung.
+      </p>
+      <h2>Fragen an den Datenschutzbeauftragten</h2>
+      <p>
+        Wenn Sie Fragen zum Datenschutz haben, schreiben Sie uns bitte eine
+        E-Mail oder wenden Sie sich direkt an die für den Datenschutz
+        verantwortliche Person in unserer Organisation:
+      </p>
+      <p>Betroffene können per E-Mail Kontakt aufnehmen.</p>
+      <p>
+        <em>
+          Die Datenschutzerklärung wurde mithilfe der activeMind AG erstellt,
+          den Experten für{" "}
+          <a
+            href="https://www.activemind.de/datenschutz/datenschutzbeauftragter/"
+            target="_blank"
+            rel="noopener"
+          >
+            externe Datenschutzbeauftragte
+          </a>{" "}
+          (Version #2020-09-30).
+        </em>
+      </p>
     </div>
   );
 }

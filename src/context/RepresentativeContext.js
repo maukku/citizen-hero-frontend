c91d20320e88363e4f65d547b1146bcd4fffd886
maukku <maurorisso17@gmail.com>
2022-07-21 19:49:17 +0200

    politicians added from backend

--- src/context/RepresentativeContext.js
+++ src/context/RepresentativeContext.js
@@ -1,60 +1,45 @@
-import { createContext, useState } from "react";
+import { createContext, useState,  useEffect} from "react";
 
 const RepresentativeContext = createContext();
 
 
-let mockObject = [
-  {
-    fullName: "Mauro Risso",
-    nationalPoliticalGroup: "lorem Ipsum party",
-    constituencyZipCodes: [22222],
-    imageUrl:
-      "https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg",
-  },
-  {
-    fullName: "Kanye West",
-    nationalPoliticalGroup: "Kanye West party",
-    constituencyZipCodes: [11111],
-    imageUrl:
-      "https://image.stern.de/31896734/t/v4/v2/w1440/r1.7778/-/25--rapper-reaktiviert-instagram-account---16-9---spoton-article-1022862.jpg",
-  },
-  {
-    fullName: "Die Mutti",
-    nationalPoliticalGroup: "CDU",
-    constituencyZipCodes: [33333],
-    imageUrl:
-      "https://cdn.prod.www.spiegel.de/images/56032691-0001-0004-0000-000001449964_w920_r1.778_fpx49_fpy40.jpg",
-  },
-  {
-    fullName: "Kanye West",
-    nationalPoliticalGroup: "Kanye West party",
-    constituencyZipCodes: [11111, 22222],
 
-    imageUrl:
-      "https://image.stern.de/31896734/t/v4/v2/w1440/r1.7778/-/25--rapper-reaktiviert-instagram-account---16-9---spoton-article-1022862.jpg",
-  },
-  {
-    fullName: "Die Mutti",
-    nationalPoliticalGroup: "CDU",
-    constituencyZipCodes: [33333],
-    imageUrl:
-      "https://cdn.prod.www.spiegel.de/images/56032691-0001-0004-0000-000001449964_w920_r1.778_fpx49_fpy40.jpg",
-  },
-  {
-    fullName: "Mauro Risso",
-    nationalPoliticalGroup: "lorem Ipsum party",
-    constituencyZipCodes: [22222],
-    imageUrl:
-      "https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg",
-  },
-];
+
 
 //dataList.push(mockObject);
 //});
 
 export function RepresentativeProvider({ children }) {
+
+
+
+
+
+
   //all methods and states
-  const [representatives, setRepresentative] = useState(mockObject);
+  const [representatives, setRepresentative] = useState([]);
+useEffect(()=>{
+getData();
+}, [])
+
+
+const url = "https://polar-mountain-34312.herokuapp.com/representatives";
+ const getData=()=>{
+fetch(url).then((res)=>res.json())
+.then((data)=>{
+let representativesData = data.map((item)=>({
+fullName:item.fullName,
+nationalPoliticalGroup: item.nationalPoliticalGroup,
+constituencyZipCodes: item.constituencyZipCodes,
+imageUrl: item.imageUrl,
+
+}));
+setRepresentative(representativesData.slice(0,20));
+}
+
+)};
+
+
 
   return (
     <RepresentativeContext.Provider

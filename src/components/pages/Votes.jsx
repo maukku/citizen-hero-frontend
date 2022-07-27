95fde102928e98f8dddb62ed9df4455e500699cb
maukku <maurorisso17@gmail.com>
2022-07-27 19:49:43 +0200

    vote container added

--- src/components/pages/Votes.jsx
+++ src/components/pages/Votes.jsx
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BlueButton from "../shared/BlueButton";
 import { Link } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
+import Divider from "@mui/material/Divider";
 
 function Votes() {
+  const [votesList, setVotesList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  async function fetchVotes() {
+    const url = "https://polar-mountain-34312.herokuapp.com/votes";
+    const response = await fetch(url);
+    const votes = await response.json();
+    setVotesList(votes);
+    setLoading(true);
+  }
 
-async function fetchVotes(){
-  const url = "https://polar-mountain-34312.herokuapp.com/votes" ;
-const response  = await fetch(url)
-const votes = await response.json();
-
-return votes;
-
-}
-
-fetchVotes().then(votes => {
-  votes;
-})
-
-
+  useEffect(() => {
+    fetchVotes();
+  }, []);
 
   return (
     <div className="BasicContainer">
@@ -27,7 +27,21 @@ fetchVotes().then(votes => {
       </Link>
 
       <div className="Votes-Container">
-        <p>Some Votes</p>
+        {loading ? (
+          votesList.map((item) => {
+            return (
+              <div className="Vote-Item">
+                <div className="Vote-Title">{item.title}</div>
+                <div className="Vote-Description">{item.description}</div>
+                <div className="Vote-Date">{item.date}</div>
+                <br />
+                <Divider></Divider>
+              </div>
+            );
+          })
+        ) : (
+          <CircularProgress />
+        )}
       </div>
     </div>
   );
